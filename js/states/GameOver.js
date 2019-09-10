c0e8d41ed767dea992d417f35330de5778aba116
tali97 <335271710@qq.com>
2019-09-10 10:54:46 -0600

    change spacebar to f in game over state

--- js/states/GameOver.js
+++ js/states/GameOver.js
@@ -3,13 +3,13 @@ var GameOver = function(game){};
 GameOver.prototype = {
 	create:function(){
 		var overText = game.add.text(game.width/3,game.height/3,'Game Over',{font:'Helvetica',fontSize:'48px',fill:'#fff'});
-		var restartText = game.add.text(game.width/2,game.height/2+256,'Press SPACEBAR to Restart',{font:'Helvetica',fontSize:'48px',fill:'#fff'});
+		var restartText = game.add.text(game.width/2,game.height/2+256,'Press F to Restart',{font:'Helvetica',fontSize:'48px',fill:'#fff'});
 		var scoreText = game.add.text(game.width/2,game.height/2,'Your Score: '+score,{font:'Helvetica',fontSize:'48px',fill:'#fff'});	
 		restartText.anchor.set(0.5);
 		scoreText.anchor.set(0.5);
 	},
 	update:function(){
-		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+		if(game.input.keyboard.isDown(Phaser.Keyboard.F)){
 			game.state.start('Play');
 			count = 5;
 			score = 0;
